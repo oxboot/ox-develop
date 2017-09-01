feef124efdd50e773aaec28940cb323e7058d351
Zorca <vs@zorca.org>
2017-09-01 09:26:44 +0300

    Colored console output added

--- src/functions/ox-utils.php
+++ src/functions/ox-utils.php
@@ -2,9 +2,11 @@
 
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 function ox_echo_info($message) {
-    echo '<info>' . $message . '</info>' . PHP_EOL;
+    $output = new ConsoleOutput();
+    $output->writeln('<info>' . $message . '</info>' . PHP_EOL);
 }
 
 function ox_console($command) {
