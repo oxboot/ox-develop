511ef7596962208dbafdbe710523c098a521af32
Zorca <vs@zorca.org>
2017-09-04 13:18:40 +0300

    Template fucntion fix

--- src/functions/ox-template.php
+++ src/functions/ox-template.php
@@ -1,9 +1,6 @@
 <?php
 
-use Mustache_Engine;
-use Mustache_Loader_FilesystemLoader;
-
 function ox_template($template, $data) {
     $m = new Mustache_Engine(['loader' => new Mustache_Loader_FilesystemLoader(OX_DIR . '/../templates')]);
-    $m->render($template, $data);
+    return $m->render($template, $data);
 }
